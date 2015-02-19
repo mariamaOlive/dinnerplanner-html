fc24abb353745f9da68a44bd8b1b34d11e36ec40
li sheng <lisheng@n156-p234.kthopen.kth.se>
2015-02-19 16:57:30 +0100

    dropdownMenu success

--- js/view/selectDishCtrl.js
+++ js/view/selectDishCtrl.js
@@ -1,11 +1,12 @@
 var SelectDishCtrl=function(view, model){
 var dropdownList = view.dropdownMenu.find(".dropdown-menu li a");
-var dropdownText = view.dropdownMenu.find(".btn:first-child");
+var dropdownText = view.dropdownMenu.find(".btn");
 	
 	dropdownList.click(function(){
-		dropdownText.text($(this).text());
-		dropdownText.val($(this).text());
-
+		dropdownText.html($(this).text()+"&nbsp; &nbsp; &nbsp; &nbsp;\
+		 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<span class='caret'></span>");
+		view.loadDishes($(this).text());
 	});
+
 
 };
--- js/view/selectDishView.js
+++ js/view/selectDishView.js
@@ -6,11 +6,13 @@ var SelectDishView= function (container, model) {
 
 
 
-	var loadDishes= function(){
+	this.loadDishes= function(dishType){
 
 		//Get all the dishes by the type of course and loads on the screen 
 		//ATTENTION: Modify to get the information of the dropbox menu
-		var dishesByType= model.getAllDishes("main dish");
+		var dishesByType= model.getAllDishes(dishType);
+
+		dishesOptions.html("");
 		
 			for(var i=0; i<dishesByType.length; i++){
 				dishesOptions.append( "<div class='col-md-3'>\
@@ -22,9 +24,17 @@ var SelectDishView= function (container, model) {
 							      </div>\
 							      </div>\
 							    </div>");
+
 			}
+	
+
 	}
 
-	loadDishes();
+     //this.update= function(){
+		//loadDishes();
+	//}
+
+	//model.addObserver(this);
+	this.loadDishes("starter");
 
 }
