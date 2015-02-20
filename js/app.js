97c479750395e6399c107c668081ec117af8c893
li sheng <lisheng@n156-p234.kthopen.kth.se>
2015-02-20 10:29:33 +0100

    merge html

--- js/app.js
+++ js/app.js
@@ -4,16 +4,16 @@ $(function() {
 	
 	//And create the needed controllers and views
 	//var exampleView = new ExampleView($("#exampleView"),model);
-	var selectDishView = new SelectDishView($("body"),model);
-	var numberGuestsView =  new NumberGuestsView($("body"),model);
-	//var dinnerPreaparationView= new DinnerPreparationView($("#dinnerPreparation"), model);
+	//var selectDishView = new SelectDishView($("body"),model);
+	//var numberGuestsView =  new NumberGuestsView($("body"),model);
+	var dinnerPreaparationView= new DinnerPreparationView($("#dinnerPreparation"), model);
 	//var dinnerOverviewView= new DinnerOverviewView($("#dinnerOverview"), model);
 	//var recipeDetailsView= new RecipeDetailsView($("#recipeDetails"), model);
     //test comment
 	//Instanciating the controllers
 	//HAHAFISH 
 	//it mariama here
-	var numberGuestsCtrl =  new NumberGuestsCtrl(numberGuestsView, model);
-	var selectDishCtrl= new SelectDishCtrl(selectDishView,model);
+	//var numberGuestsCtrl =  new NumberGuestsCtrl(numberGuestsView, model);
+	//var selectDishCtrl= new SelectDishCtrl(selectDishView,model);
 
 });
